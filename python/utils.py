36be45a604c751195908105901763fac067ebeeb
David Lowry-Duda <david.j.lowry@gmail.com>
2018-12-01 16:09:30 +0000

    Python day 1

--- python/utils.py
+++ python/utils.py
@@ -16,7 +16,7 @@ def _read_file(filename):
 
 def read_input(day):
     """
-    Returns file to input for `day`.
+    Returns file of input for `day`.
 
     The input is assumed to be in `inputs/input<day>.txt`, where <day> is
     replaced with the number of the day.
@@ -24,3 +24,10 @@ def read_input(day):
     Note that it's likely that `read_input(<day>).read()` will be called.
     """
     return _read_file("inputs/input{}.txt".format(day))
+
+
+def input_lines(day):
+    """
+    Saves the inputfile from day `day` line-by-line, stripping linebreaks.
+    """
+    return list(map(lambda x: x.strip(), read_input(day).readlines()))
